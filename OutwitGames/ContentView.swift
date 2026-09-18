08e9ea214671d9fd7d284fcafe909e542376d35d
ayushyadav-lab <ayush.dms.1415@gmail.com>
2026-09-18 09:41:09 +0530

    Initial Commit

---
+++ OutwitGames/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OutwitGames
+//
+//  Created by Ayush yadav on 18/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OutwitGames/OutwitGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutwitGamesApp.swift
+//  OutwitGames
+//
+//  Created by Ayush yadav on 18/09/26.
+//
+
+import SwiftUI
+
+@main
+struct OutwitGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
